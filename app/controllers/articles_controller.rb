2c37870121aea124120884a72cfbc149263b319e
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-10-30 16:47:03 +0200

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  # Return all the articles
+  def index
+    @articles = Article.all
+  end
+
+  # Return an specific article from their id
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # New item
+  def new
+    # @article = Article.new(article_params)
+    @article = Article.new
+  end
+
+  # Creates a new article with the values title and body
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  
+  get 'welcome/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Is possible to define all the verbs
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # Or use the keyword resources
+  resources :articles
+end
