a30b0a6fdbbc9dd4f09f9bd587d0984396eb9662
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-10-30 17:59:35 +0200

    dependent assosiation from our articles to our comments once they are delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
     include Visible
   
-    has_many :comments
+    # If you delete an article, its associated comments will also need to be deleted,
+    # otherwise they would simply occupy space in the database. 
+    # Rails allows you to use the dependent option of an association to achieve this.
+    has_many :comments, dependent: :destroy
   
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
